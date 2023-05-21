392e51c65b91df7c5c7eba0c716468622a75f40b
entero <133282950+entero8188@users.noreply.github.com>
2023-05-20 22:48:27 -0400

    Update verlet.cc
    le saque los cout con weas q habia puesto para ver donde moria el programa xD

--- verlet.cc
+++ verlet.cc
@@ -2,11 +2,7 @@
 #include <cmath>
 #include<fstream>
 
-#include "../../../templates/complejos.h"
-#include "../../../templates/fracciones.h"
-#include "../../../templates/dual.h"
-#include "../../../templates/matriz.h"
-#include"../../../templates/spline.h"
+
 
 using namespace std;
 
@@ -57,9 +53,9 @@ class integrador{
 
 
         long double f_1(int i){
-            cout<<"vaginaaa"<<endl;
+            
             long double x=this->x_i+i*this->dx;
-            cout<<"por favor"<<endl;
+            
             return -x*this->lista_f[i]+x*g(x);
         }
 
@@ -85,21 +81,21 @@ class integrador{
 
 
         void calcular(long double phi){
-            cout<<"pene1"<<endl;
+            
             this->lista_f.push_back(phi);
-            cout<<"pene2"<<endl;
+            
 
             long double phi_1=f_1(0);
-            cout<<"pene3"<<endl;
+            
             this->x.push_back(x_i+this->dx);
 
-            cout<<"pene4"<<endl;
+            
             this->lista_f_1.push_back(phi_1);
-            cout<<"pene5"<<endl;
+            
             this->lista_f.push_back(phi+phi_1*this->dx);
-            cout<<"pene6"<<endl;
+            
             this->lista_f_1.push_back(f_1(1));
-            cout<<"pene7"<<endl;
+            
 
             for(int i = 1; i<this->n; i++){
                 long double step=2*this->lista_f[i]-lista_f[i-1]+this->f_2(i)*this->dx*this->dx;
@@ -124,17 +120,17 @@ class integrador{
 
 int main(){
 
-    cout<<"hola1"<<endl;
+    
     srand(time(0));
-    cout<<"hola2"<<endl;
+    
 
     integrador A=integrador(2,3,10);
 
     int a=A.lista_f.size()==A.lista_f_1.size();
-    cout<<a<<endl;
+    
 
     A.calcular(1.20205);
-    cout<<"hola4"<<endl;
+   
     A.guardar_integracion();
 
 
