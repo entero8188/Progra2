302e13aa76d74713d76d283568b0fc52253567ee
DarkToledo <133540915+DarkToledo@users.noreply.github.com>
2023-05-22 23:23:51 -0400

    Create rk4.h
    lo hizo el ayudante yo solo copie la imagen no c nada ayuda

---
+++ rk4.h
@@ -0,0 +1,136 @@
+#ifndef RK4_H
+#define RK4_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+//clase del ayudante
+//clase de rk4 para ec diferenciales de varias dimensiones
+
+template <class T>
+
+class RK4{
+
+ private:
+  T dt = 0.1; //delta t
+  T t_i; //tiempo inicial
+  T t_f; //tiempo final
+  
+  //condiciones iniciales en t_i
+  vector<T> posiciones_iniciales; //(x, y, z)
+  //ecuaciones diff a resolver
+  vector<T> (*derivadas)(vector<T>); //(t, x, y, z)
+
+ public:
+  //constructor generico
+  RK4(){};
+  //el fr fr
+  RK4(vector<T> (*derivadas)(vector<T>),
+      vector<T> posiciones_iniciales, T t_i,  T t_f,  T dt){
+
+    this-> dt = dt;
+    this-> t_i = t_i;
+    this-> t_f = t_f;
+    this-> posiciones_iniciales = posiciones_iniciales;
+    this-> derivadas = derivadas;
+  };
+
+  
+  //funcion que resulve la edo introducida al inicializar la clase
+  void rk4(string nombre){
+    T t = t_i;
+    vector<T> posiciones = posiciones_iniciales;
+    /*analogo a:
+      x = posiciones_iniciales[0]
+      y = posiciones_iniciales[0]
+      ...*/
+    // guarda los datos en "nombre"
+    ofstream rk4;
+    rk4.open(nombre);
+
+    //numero variables
+    int n_var = posiciones_iniciales.size();
+
+    //guardo el valor de los k1, k2, k3, k4 para cada variable
+    //analogo a crear k1_x, k1_y, ..., k2_x, k2_y, ..., k4_n
+    vector<T> k1(n_var);
+    vector<T> k2(n_var);
+    vector<T> k3(n_var);
+    vector<T> k4(n_var);
+
+    /*valores en los cuales está evaluando k1, k2, k3, k4 para 
+      cada componente incluye la variable independiente (t),
+      por ello tiene tamaño n_var+1*/
+
+    vector<T> posicion_k1(n_var+1);
+    vector<T> posicion_k1(n_var+1);
+    vector<T> posicion_k1(n_var+1);
+    vector<T> posicion_k1(n_var+1);
+
+    //comienza a calcular la solucion de edo hasta t_f
+    while(t<t_f){
+      //se guardan los datos de rk4
+      rk4 << t << " ";
+      for(int i = 0; i < n_var; i++){
+	rk4 << posiciones[i] << " ";
+      }
+      rk4 << endl;
+
+      //pongo los valores independientes en la coordenada 0
+      posicion_k1[0] = t;
+      posicion_k2[0] = t+dt/2;
+      posicion_k3[0] = t+dt/2;
+      posicion_k4[0] = t+dt;
+
+      //calculo los valores de posicion_k1 para todas las variables
+      //dado que ya puse el tiempo, comienzo desde i+1
+      for(int i = 0; i < n_var; i++){
+	posicion_k1[i+1] = posiciones[i];
+      }
+      k1 = derivadas(posiciones_k1);
+
+      //sigo calculando los valores que van evaluados en k2, k3 y k4
+      //siguiendo los pasos del metodo runge-kutta-4
+      for(int i = 0; i < n_var; i++){
+	posicion_k2[i+1] = posiciones[i]+k1[i]*dt/2;
+      }
+      k2 = derivadas(posiciones_k2);
+
+      //calculo posicion_k3 y k3
+      for(int i = 0; i < n_var; i++){
+	posicion_k3[i+1] = posiciones[i]+k2[i]*dt/2;
+      }
+      k3 = derivadas(posiciones_k3);
+      
+      //calculo posicion_k4 y k4
+      for(int i = 0; i < n_var; i++){
+	posicion_k4[i+1] = posiciones[i]+k3[i]*dt/2;
+      }
+      k4 = derivadas(posiciones_k4);
+
+      /*lo anterior es analogo (para el caso n_var=3) a lo siguiente:
+	k1 = derivadas(t, x, y, z);
+	k2 = derivadas(t+dt/2., x + k1[0]*dt/2., y 
+	+ k1[1]*dt/2., z + k1[2]*dt/2.);
+	k3 = derivadas(t+dt/2., x + k2[0]*dt/2., y 
+	+ k2[1]*dt/2., z + k2[2]*dt/2.);
+	k4 = derivadas(t+dt/2, x + k3[0]*dt, y 
+	+ k3[1]*dt, z + k3[2]*dt);
+	(notese que habría que definir de forma distinta la función
+	derivadas a como se hizo en la parte sin comentar)*/
+
+      /*encontramos las siguientes posiciones para cada
+	componente realizando un promedio sobre k1, k2, k3, k4,*/
+      for(int i = 0; i<n_var; i++){
+	posiciones[i] = posiciones[i] + 1./6.*
+	  (k1[i]+2*k2[i]+2*k3[i]+k4[i])*dt;
+      }
+      t = dt;
+    }
+    rk4.close();
+  }
+};
+
+#endif
