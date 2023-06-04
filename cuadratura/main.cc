ed7d439c776e63e8d311d27dea869ffeea4fa2f4
Axel Reyes <132612509+ReyesAxel@users.noreply.github.com>
2023-06-04 08:47:55 -0400

    Update main.cc

--- cuadratura/main.cc
+++ cuadratura/main.cc
@@ -10,7 +10,6 @@ int main(){
   double N=10;
 
   Cuadratura ec(a,b,N,f);
-  int fac = 1;
 
   cout << "trapezoidal: " << endl;
   ec.trapezoidal();
@@ -20,15 +19,17 @@ int main(){
   ec.richardson(10000);// esta cosa si es muy baja el error es super alto
   cout << "romberg: " << endl;
   ec.romberg();
+  cout << "Gauss: " << endl;
+  ec.gauss();
 
   return 0;
 }
 
 double f(double x){
-  return (x*x*x*x);
+  return (x*x*x*x); // Con polinomios de orden 4 gauss se cae a pedazos.
+  //return (x*x*x+2*x*x+3*x+2); // con ordenes menores como estos funciona bien.
   //return (x*x + 6*x -8);
 }
 
 
 // el resultado analitico de integrar x^4 entre 0 y 5 es 625.
-// el N aqui esta muy bajo, estaba probando..
